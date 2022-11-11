3a2b4002ef1ce2dd7e25737654ce305d93b54cb3
seong9566 <dlgustjd9566@gmail.com>
2022-11-11 15:13:44 +0900

    그리드 뷰 사용해서 other 탭바 생성

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/models/tab.dart';
 
 //바텀 앱바의 ... 을 클릭시 사용됨.
 class MoreScreen extends StatelessWidget {
@@ -6,8 +7,26 @@ class MoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("MoreScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("더보기"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: GridView.count(
+          crossAxisCount: 4,
+          children: List.generate(
+              tabs.length,
+              (index) => Column(
+                    children: [
+                      Icon(tabs[index].icon),
+                      SizedBox(height: 5),
+                      Text(tabs[index].text),
+                    ],
+                  )),
+        ), // 한 줄에 4개씩
+      ),
     );
   }
 }
