6d4610ae3dfffadffe86d0693b0962d88337c182
seong9566 <dlgustjd9566@gmail.com>
2022-11-11 14:29:25 +0900

    채팅방에 TimeLine 넣기

--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/time_line.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ChatRoomScreen extends StatefulWidget {
@@ -11,28 +12,42 @@ class ChatRoomScreen extends StatefulWidget {
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFFb2c7da),
-      child: Scaffold(
-        backgroundColor: Colors.transparent, // Scaffold가 배경을 가리지 않게 처리 - 중요
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text(
-            "홍길동",
-            style: Theme.of(context).textTheme.headline6,
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFb2c7da),
+        child: Scaffold(
+          backgroundColor: Colors.transparent, // Scaffold가 배경을 가리지 않게 처리 - 중요
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text(
+              "홍길동",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            actions: [
+              Icon(
+                FontAwesomeIcons.search,
+                size: 20,
+              ),
+              SizedBox(width: 25),
+              Icon(
+                FontAwesomeIcons.bars,
+                size: 20,
+              ),
+              SizedBox(width: 25),
+            ],
           ),
-          actions: [
-            Icon(
-              FontAwesomeIcons.search,
-              size: 20,
-            ),
-            SizedBox(width: 25),
-            Icon(
-              FontAwesomeIcons.bars,
-              size: 20,
-            ),
-            SizedBox(width: 25),
-          ],
+          body: Column(
+            children: [
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TimeLine(time: "2022년 11월 11일 금요일"),
+                  ],
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
